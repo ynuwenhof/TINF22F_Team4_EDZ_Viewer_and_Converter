94c948f28633ad9be623b0acbe8a06fd3526d2e4
fabiansig <fabian.sigmund1@gmail.com>
2023-09-29 09:52:31 +0200

    ng new

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TINF22F_Team4_EDZ_Viewer_and_Converter';
+}
