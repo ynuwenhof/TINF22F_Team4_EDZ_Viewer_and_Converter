ccf5db9a9ce7dbe69330495aded7879f8d372158
Mustafa Sahin <mustafasahin.6123@gmail.com>
2024-03-08 13:29:33 +0100

    Add SQLite database

--- Backend/app.js
+++ Backend/app.js
@@ -6,3 +6,23 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 const port = process.env.PORT;
+
+app.post("/api/set/file", async (req, res) => {
+    const body = req.body;
+    try {
+
+    }
+    catch (e) {
+
+    }
+});
+
+app.get("/api/get/file", async (req, res) => {
+    const body = req.body;
+    try {
+
+    }
+    catch (e) {
+
+    }
+});
