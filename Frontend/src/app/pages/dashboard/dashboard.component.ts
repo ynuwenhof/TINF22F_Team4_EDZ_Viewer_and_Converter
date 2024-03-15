757f9d395119595b63f4b07ea4ac72e6129007cb
Sw0rd74nd <luigi.xu2001@gmail.com>
2024-03-15 16:55:40 +0100

    feat(ui): add cards to dashboard

--- Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -7,23 +7,39 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
   loading = false;
+  cards: any;
+  cardsInQueue = 0;
 
-  cards = [
-    {
-      title: "Bauteil",
-      description: "Beschreigung?",
-      size: "xxMB",
-      date: new Date(),
-      partCount: 10,
-      id: "xxx"
-    },
-    {
-      title: "Bauteil2",
-      description: "Beschreigung?",
-      size: "xxxMB",
-      date: new Date(),
-      partCount: 3,
-      id: "yyy"
-    },
-  ];
+  constructor() {
+    setTimeout(() => {
+      this.cardsInQueue = 3;
+      this.cards = [
+        {
+          title: "Bauteil",
+          description: "Beschreigung?",
+          size: "xxMB",
+          date: new Date(),
+          partCount: 10,
+          id: "xxx"
+        },
+        {
+          title: "Bauteil2",
+          description: "Beschreigung?",
+          size: "xxxMB",
+          date: new Date(),
+          partCount: 3,
+          id: "yyy"
+        },
+        {
+          title: "Bauteil2",
+          description: "Beschreigung?",
+          size: "xxxMB",
+          date: new Date(),
+          partCount: 3,
+          id: "yyy"
+        },
+      ];
+
+    }, 400)
+  }
 }
